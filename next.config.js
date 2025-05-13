e7278a4a7efa77fb7c8a512b00cc52c7063e7fbe
Your Name <your-email@example.com>
2025-05-13 18:17:40 +0100

    Fixes

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
    
     // responseLimit: '50mb', // Adjust as needed
   },
-  output: ".next", // Updated from experimental.outputStandalone
+  output: "standalone", // Updated from experimental.outputStandalone
   async headers() {
     return [
       {
